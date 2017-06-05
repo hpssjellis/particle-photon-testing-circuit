c3c0872f403a8aa173132eed1a3f674d70adade2
Jeremy Ellis <keyfreemusic@gmail.com>
2017-06-05 12:41:44 -0700

    uart serial working

--- working-master.ino
+++ working-master.ino
@@ -16,17 +16,31 @@
 
 void setup() {
     Serial1.begin(9600);
-    Serial1.write('A');
-    
-    if (Serial1.available() > 0) {
-        int incomingFromSlave = Serial.read();
-        if (incomingFromSlave == 'B') {  
-            Particle.publish("uART Serial working", String(incomingFromSlave), 60, PRIVATE);
-        } 
-    }
+
+
 }
 
 void loop() {
 
-
+    
+    if (Serial1.available() > 0) {
+        Serial1.write('A');
+        Particle.publish("uART Serial working", "Sent an A", 60, PRIVATE);
+        delay(1000);
+        delay(1000);
+    }
+        if (Serial1.available() > 0) {
+        int incomingFromSlave = Serial1.read();
+        if (incomingFromSlave == 'B') {  
+            Particle.publish("uART Serial working B = 66", String(incomingFromSlave), 60, PRIVATE);
+            delay(1000);
+        }  else {
+             Particle.publish("Serial working", "No returned value", 60, PRIVATE);
+             delay(1000);
+        }
+    } else  {
+            Particle.publish("Serial Not working", "---", 60, PRIVATE);
+            delay(1000);
+        }
+    delay(1000);
 }
