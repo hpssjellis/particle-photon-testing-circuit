17a9a01fade218639d1e26e36a7e2abbe19dbaf9
Jeremy Ellis <keyfreemusic@gmail.com>
2017-06-07 14:29:07 -0700

    Update i2c-master.ino

--- i2c-master.ino
+++ i2c-master.ino
@@ -1,25 +1,38 @@
 
-String myString ;
+int idx;
+
+
+String myOut = "________________________________"; // Note: 32 chars Max string read 
 
 void setup() {
-    Wire.begin();
+   // Serial.begin(9600);
+    Wire.begin(8);
+    Wire.onReceive(receiveEvent);
     pinMode(D7,OUTPUT);
+
 }
 
-void loop() {
-    delay(10000);
-    
-    digitalWrite(D7,HIGH);
-    Wire.beginTransmission(8);
-    
-    // only sends 32 characters but you can try more
-    myString = "123456789012345678901234567890123456";
-    Wire.write(myString);
-    Wire.endTransmission();
+void receiveEvent(int numOfBytes) {
+    idx = 0;
+    while (Wire.available()) { 
+        
+        myOut.setCharAt(idx, (char)Wire.read() );  
+        idx ++;
+    } 
+}
 
-    delay(20);
-    digitalWrite(D7,LOW);
-    Particle.publish("I2C sent, wait 10 (s)", myString ,60,PRIVATE);
-    
-    
+
+
+void loop() {            
+    Particle.publish("I2C receiving wait 10 (s)",String(idx),60,PRIVATE);
+    delay(10000);
+    if (idx != 0) {
+        digitalWrite(D7,HIGH);
+        Particle.publish("I2C data better",String(myOut),60,PRIVATE);
+        delay(1000);
+        idx = 0;
+        digitalWrite(D7,LOW);
+    }
+
+
 }
