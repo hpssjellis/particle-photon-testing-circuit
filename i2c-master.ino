15c77768e4e3bad63df0ef4a8dd675c935b95894
Jeremy Ellis <keyfreemusic@gmail.com>
2017-06-07 07:15:53 -0700

    Create i2c-master.ino

---
+++ i2c-master.ino
@@ -0,0 +1,16 @@
+// Include the required Wire library for I2C<br>#include 
+int x = 0;
+void setup() {
+  // Start the I2C Bus as Master
+  Wire.begin(); 
+}
+void loop() {
+  Wire.beginTransmission(9); // transmit to device #9
+  Wire.write(x);              // sends x 
+  Wire.endTransmission();    // stop transmitting
+  x++; // Increment x
+  if (x > 5) x = 0; // `reset x once it gets 6
+  
+        Particle.publish("I2C", String(x), 60, PRIVATE);
+  delay(2000);
+}
