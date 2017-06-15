838b1298f1afa7ab978f9680e9a3fce06c39b735
Jeremy Ellis <keyfreemusic@gmail.com>
2017-06-14 23:09:35 -0700

    Create spi-slave.ino

--- individual-serial/spi-slave.ino
+++ individual-serial/spi-slave.ino
@@ -3,6 +3,14 @@
 // MIT license
 // Use at your own risk!
 
+    // Photon to Photon SPI Setup
+    // GND to GND
+    // 3V3 to 3V3
+    // A2 to A2   SS
+    // A3 to A3   SCK
+    // A4 to A4   MISO
+    // A5 to A5   MOSI
+
 
 void slaveSelect(uint8_t state);
 void slaveCallback();
