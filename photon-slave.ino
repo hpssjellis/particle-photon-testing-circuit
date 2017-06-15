bfdb8b3ee1da02d951286e5ae195ea792d0c5135
Jeremy Ellis <keyfreemusic@gmail.com>
2017-06-14 23:08:01 -0700

    Create photon-slave.ino

--- photon-slave.ino
+++ photon-slave.ino
@@ -24,6 +24,15 @@
     // pullup 4.7 K ohm resistor from D0 to 3V3
     // pullup 4.7 K ohm resistor from D1 to 3V3
 
+    // Photon to Photon uART Setup
+    // GND to GND
+    // 3V3 to 3V3
+    // A2 to A2   SS
+    // A3 to A3   SCK
+    // A4 to A4   MISO
+    // A5 to A5   MOSI
+
+
 //uART Global variables
 
 String myUartSlaveOut = "From uART Slave";
